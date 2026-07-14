beb780a01f594908e68156fc718b86645f5e6d6f
miguellucietto7-alt <miguellucietto7@gmail.com>
2026-07-14 11:22:16 -0300

    added save and load table and free_table

--- src/table.c
+++ src/table.c
@@ -6,6 +6,16 @@
 #include "database.h"
 #include "utils.h"
 
+void insert_table(DataBase* db, Table* table)
+{
+    if (!db || table) return;
+
+    size_t index = hash(table->name) % db->cap;
+    Table* head = db->buckets[index];
+    table->next = head;
+    db->buckets[index] = table;
+}
+
 Table* search_table(DataBase* db, const char* name)
 {
     if (!db || !name) return NULL;
@@ -51,10 +61,7 @@ void create_table(DataBase* db, const char* name)
     Table* table = new_table(name);
     if (!name || !db || !table) return;
 
-    size_t index = hash(name) % db->cap;
-    Table* head = db->buckets[index];
-    table->next = head;
-    db->buckets[index] = table;
+    insert_table(db, table);
 }
 
 void insert_col(Table* table, char* col)
@@ -99,4 +106,87 @@ void define_table(DataBase* db, const char* name, TokenList* list)
     {
         insert_col(table, list->tokens[i]);
     }
+}
+
+void free_table(Table* table)
+{
+    if (!table) return;
+
+    for (size_t i = 0; i < table->rows_count; i++)
+    {
+        for (size_t j = 0; j < table->cols_count; i++)
+        {
+            free(table->rows[i][j]);
+        }
+        free(table->rows[i]);
+    }
+    for (size_t i = 0; i < table->cols_count; i++)
+    {
+        free(table->cols[i]);
+    }
+
+    free(table->name);
+    free(table);
+}
+
+void load_table(DataBase* db, const char* file_path)
+{
+    if (!file_path) return NULL;
+    FILE* file = fopen(file_path, "r");
+    Table* table = new_table(strtok(file, "\n"));
+
+    table->cols_count = strtoull(strtok(NULL, "|"), NULL, 10);
+
+    TokenList* list = tokenize(strtok(NULL, "\n"), " ");
+    if (!list) return;
+    for (size_t i = 0; i < table->cols_count; i++)
+    {
+        insert_col(table, list->tokens[i]);        
+
+    }
+    free_tokens(list);
+    
+    table->rows_count = strtoull(strtok(NULL, "|"), NULL, 10);
+
+    for (list = strtok(NULL, "|"); list != NULL; list = strtok(NULL, "|"))
+    {
+        insert_row(table, list->tokens);
+    }
+
+    fclose(file);
+}
+
+void save_table(Table* table, const char* dir_name)
+{
+
+    if (!table || !dir_name) return;
+    
+    char path[256];
+    snprintf(path, 256, "%s/%s", dir_name, table->name);
+
+    FILE* file = fopen(path, "w");
+
+    // Salva o nome
+    fprintf(file, "%s\n", table->name);
+
+    // Salva as colunas
+    fprintf(file, "%zu|", table->cols_count);
+    for (size_t i = 0; i < table->cols_count; i++)
+    {
+        fprintf(file, "%s ", table->cols[i]);
+    }
+    fprintf(file, "\n");
+
+    // Salva as rows
+    fprintf(file, "%zu|", table->rows_count);
+    for (size_t i = 0; i < table->rows_count; i++)
+        for (size_t j = 0; j < table->cols_count; j++)
+        {
+            fprintf(file, "%s ", table->rows[i][j]);
+        }
+        fprintf(file, "|");
+    
+    fprintf(file, "\n");
+
+    fclose(file);
 }
